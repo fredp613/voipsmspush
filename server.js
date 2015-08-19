0daa6c7c15a66798a962a014d5ece6e1dc75e0d8
fredp613 <fredp613@gmail.com>
2015-08-19 10:54:58 -0400

    undo

--- server.js
+++ server.js
@@ -2,13 +2,8 @@
 var express = require('express');
 var app = require("express")();
 var bodyParser = require('body-parser');
-// var User = require('./models/user_model.js');
-var mongoose = require('mongoose');
-var agent = require('./agent/_header')  
-var request = require('request');
 var User = require('./models/user_model.js');
-var Message = require('./models/message_model.js');
-var async = require('async')
+var mongoose = require('mongoose');
 
 // if(process.env.OPENSHIFT_NODEJS_PORT) {
 // 	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
@@ -94,97 +89,6 @@ app.post("/users", function (req, res) {
 app.post("/users", function (req, res) { res.send("all the HTTP verb looks the same");});
 app.patch("/users", function (req, res) { res.send("all the HTTP verb looks the same");});
 app.del("/users", function (req, res) { res.send("all the HTTP verb looks the same");});
-
-
-getUserList()
-
-function getUserList() {
-	User.find({}, function(err, users) {
-		if (err) throw err;
-		if (users.length > 0) {
-			 getUserMessages(users)	
-		} else {
-			setTimeout(getUserList, 3000)
-		}		 		 	
-	});
-}
-
-function getUserMessages(users) {
-
-	 async.eachSeries(users, function (user, callback) {
-
-		  var params = {
-			  email: user.email,
-	 	  	pwd: user.password,
-	 	  	token: user.device_token
-		  }		  		  		  
-			  messageRequest(params)		  			  		 
-		  callback(); // Alternatively: callback(new Error());
-		}, function (err) {
-		  if (err) { throw err; }		  	 
-		});
-}
-
-function messageRequest(params) {
-	  
-		var url = "https://voip.ms/api/v1/rest.php?api_username="+ params.email +"&api_password="+ params.pwd +"&method=getSMS&type=1&limit=5"					 					
-		request(url, function(err, response, body){ 
-			console.log(body)
-			if (!err) {
-				var responseObject = JSON.parse(body);			        					  	
-		  	var messages = responseObject.sms	
-				if (responseObject["status"] == "success") /** && message does not exist in db  **/ {					  	    						  								  							
-					async.eachSeries(messages, function(message, callback){					
-						saveMessage(message, params.token)
-						callback();
-
-					}, function(err) {
-						if (err) throw err;					
-						setTimeout(getUserList, 3000)
-					})
-				} else {
-					setTimeout(getUserList, 3000)	
-				}
-			} else {
-				setTimeout(getUserList, 3000)
-			}			
-		});
-}
-	
-function saveMessage(message, token) {
-	Message.findOne({ message_id: message.id}, function (err, doc){	  
-	  if (!doc) {	  	
-	  	var m = new Message({
-			  message_id: message.id, 
-			  did: message.did,
-			  contact: message.contact, 
-			  message: message.message,
-			  created_at: new Date().toLocaleString(),
-			  updated_at: new Date().toLocaleString()   
-			});	
-	  	console.log(token)
-		  m.save(function(e) {
-			  	if (e) throw e;		
-			  	 agent.createMessage()			  	 
-					  .device(token)
-					  .alert(message.message)
-					  .set('contact', message.contact)
-					  .set('did', message.did)
-					  .set('id', message.id)
-					  .set('date', message.date)
-					  .set('message', message.message)					  
-					  .send();		  
-					  console.log(token)																												
-			});
-	  } else {			    							  		  	
-	  }	 											  		
-	});	
-//contact, id, date, message, did
-}
-
-
-
-
 
 
 
