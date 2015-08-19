90d0acecb934bf04c4fc2673bca9bb3a31b2fb40
fredp613 <fredp613@gmail.com>
2015-08-19 16:39:54 -0400

    mongod issue

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var mongoose = require('mongoose');
 var connection_string = 'mongodb://localhost/voipapidb';
 if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  connection_string = "admin" + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
