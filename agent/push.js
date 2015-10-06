ad7bd14eb7d2206ae309f4e76820001807203f28
fredp613 <fredp613@gmail.com>
2015-10-05 23:47:18 -0400

    t

--- agent/push.js
+++ agent/push.js
@@ -113,13 +113,13 @@ function PushLoop() {};
 	  					 											"did" : message.did,
 	  					 											"id" : message.message_id,
 	  					 											"date" : message.date,
-	  					 											"message" : JSON.stringify(message.message).replace(/'/g, "'")
+	  					 											"message" : ""+message.message+"" //JSON.stringify(message.message).replace(/'/g, "'")
 	  					 										}	  					 										
 			  					 								var note = new apns.Notification();
 			  					 								var myDevice = new apns.Device(message.device_token);
 																	note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
 																	note.badge = 3;																	
-																	note.alert = message.contact + ": " + JSON.stringify(message.message).replace(/'/g, "'");
+																	note.alert = message.contact + ": " + ""+message.message+""
 																	note.payload = payload;
 																	apnsConnection.pushNotification(note, myDevice);																	
 																}
